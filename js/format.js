1caf0c0c88515b237acee8a7692ba842b72d7b47
sugimoto <sugimoto@softem.com>
2016-08-19 17:46:25 +0900

    ddd

---
+++ js/format.js
@@ -0,0 +1,9 @@
+function init_format(){
+   //$('ul.tabs').tabs();
+  var vm_format = new Vue({
+    el: '#format',
+    data: {
+      name: 'Name'
+    }
+  })
+}
--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 function init_index(){
-
+   //$('ul.tabs').tabs();
+  new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue.js!'
+    }
+  })
 }
